7d39b9d38a9cd745003ae1d318834ef57b336780
George Mylonas <geomylonas@gmail.com>
2022-01-14 22:22:28 +0200

    created uploadfile method

--- WebApplication/Controllers/PicturesController.cs
+++ WebApplication/Controllers/PicturesController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.Http.Description;
@@ -85,8 +87,67 @@ namespace WebApplication.Controllers
             return CreatedAtRoute("DefaultApi", new { id = picture.Id }, picture);
         }
 
-        // DELETE: api/Pictures/5
-        [ResponseType(typeof(Picture))]
+
+
+        public async Task<IHttpActionResult> Uploadfile()
+        {
+            try
+            {
+                var fileuploadPath = "..\\..\\Files";
+
+                var provider = new MultipartFormDataStreamProvider(fileuploadPath);
+                var content = new StreamContent(HttpContext.Current.Request.GetBufferlessInputStream(true));
+                foreach (var header in Request.Content.Headers)
+                {
+                    content.Headers.TryAddWithoutValidation(header.Key, header.Value);
+                }
+                await content.ReadAsMultipartAsync(provider);
+                string uploadingFileName = provider.FileData.Select(x => x.LocalFileName).FirstOrDefault();
+                string originalFileName = String.Concat(fileuploadPath, "\\" + (provider.Contents[0].Headers.ContentDisposition.FileName).Trim(new Char[] { '"' }));
+                var filename = provider.Contents[0].Headers.ContentDisposition.FileName;
+                if (File.Exists(originalFileName))
+                {
+                    File.Delete(originalFileName);
+                }
+                File.Move(uploadingFileName, originalFileName);
+                Picture picture = new Picture();
+                picture.Url = filename;
+                UnitOfWork.Pictures.Add(picture);
+                await  UnitOfWork.Complete();
+
+
+                //Stotefile sf = new Stotefile();
+                //sf.File = filename;
+                //DB.Stotefiles.Add(sf);
+                //DB.SaveChanges();
+                return Ok("Uploaded Successfuly");
+                
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, "There was and error during upload");
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // DELETE: api/Pictures/5
+            [ResponseType(typeof(Picture))]
         public async Task<IHttpActionResult> DeletePicture(int id)
         {
             Picture picture = await UnitOfWork.Pictures.Get(id);
