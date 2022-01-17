058d834cc8927ebae6621245cf76eefb0631c45a
Chrisostomos Valsamakis <chrisvals62@gmail.com>
2022-01-17 04:20:22 +0200

    FrotnEnd Added Purchase Details

--- FrontEnd/src/pages/purchases/Purchases.js
+++ FrontEnd/src/pages/purchases/Purchases.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import { Link } from "react-router-dom";
+import "./Purchases.css";
 
 
 
@@ -30,25 +31,32 @@ export default function Purchases(){
     
 
     return(
-        <div>
+        <>
+        <div id="title">
+            <h2>Tickets</h2>
+        </div>
+        <div id="tickets">
             {data.map(p=>{
 
                 return(
 
                     <div className="purchase" key={p.TicketCodes[0]}>
-                        <h2>Purchase Details</h2>
                         <h4>Title</h4>
                         {p.EventTitle}
                         <h4>Address</h4>
                         {p.EventAddress}
-                        <h4>Purchase</h4>
-                        {p.PurchaseDate}
+                        <h4>Date Of Purchase</h4>
+                        {p.PurchaseDate.slice(0,10)}
                         <h4>Ticket Quantity</h4>
                         {p.TicketQuantity}
-                </div>
+                        <Link to={`/events/info/${p.EventId}`}>
+                        <button className="toEvent">Check Event</button>
+                        </Link>
+                    </div>
                     )
                 })
             }
         </div>
+        </>
     )
 }   
